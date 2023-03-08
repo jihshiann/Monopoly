d0e7efa21383e66cca7e46626398be9fca8593ca
joeytsai <joeytsai@accuvally.com>
2023-03-08 22:42:10 +0800

    單輪基本流程完成

--- Monopoly/Monopoly.py
+++ Monopoly/Monopoly.py
@@ -1,45 +1,99 @@
-﻿import enum
+﻿from textwrap import indent
 from paraLib import *
+import random
+import json
 
-# 執行模式
-class ExecutionMode(enum.Enum):
-    fullyAuto = 0 #連同參數設定皆為自動
-    semiAuto = 1 #使用者輸入參數
 
-_executionMode= ExecutionMode.fullyAuto
+#取得所有玩家資料
+players = getPlayers()
+#取得所有土地資料
+lands = getLands()
+#取得各項費率
+rate_dict = getRateDict()
 
-# FUN:初始化執行模式
-def initialize():
-    print("選擇執行模式")
-    print(" 0: 全自動模式(包含參數設定)")
-    print(" 1: 由使用者輸入參數")
+loserCount = 0
+round = 0
 
-    #print("五秒內未輸入則為自動模式")
-    #start_time = time.time()
-    #while (time.time() - start_time) < 5:
-    #    if m:=input("請輸入0 or 1"):
-    #        userInput = m
-    #        break
-    #    else:
-    #        userInput = "0"
+#遊玩中玩家>1位
+while loserCount < len(players)- 1:
+    round += 1
+    for player in players:
+        #1 = lose
+        if player.status == 1:
+            continue
+        #0 = play
+        if player.status == 0:
+            #向前走1~4步
+            player.location = player.location + random.randint(1, 4)
+            if player.location > len(lands):
+                player.location = player.location % len(lands)
+            arriveLand = lands[player.location-1]
+            #判斷土地持有權
+            if arriveLand.owner == 'None':
+                #判斷現金是否足夠購買
+                if player.money >= arriveLand.price:
+                    player.money -= arriveLand.price
+                    arriveLand.owner = player.id
+                    arriveLand.level += 1
+                    arriveLand.invest += arriveLand.price
+            
+            elif arriveLand.owner == player.id:
+                #公式: price * rate * level
+                upgradeCost = rate_dict['upgrade'] * arriveLand.price * arriveLand.level
+                #判斷現金是否足夠升級
+                if player.money >= upgradeCost:
+                    player.money -= upgradeCost
+                    arriveLand.level += 1
+                    arriveLand.invest += upgradeCost
 
-    userInput=input("請輸入0 or 1\n")
-    if userInput == "0":
-        _executionMode = ExecutionMode.fullyAuto
-    elif userInput == "1":
-        _executionMode = ExecutionMode.semiAuto
-    else :
-        print("終止程式")
-        exit()
+            else:
+                #公式 price * rate * level
+                toll = rate_dict['toll'] * arriveLand.price * arriveLand.level
+                payee = players[arriveLand.owner-1] #接受過路費對象(地主)
+                #判斷現金是否足夠付過路費
+                if player.money >= toll:
+                    payee.money += toll
+                    player.money -= toll
+                else:
+                    #先付清現金
+                    payee.money += player.money
+                    toll -=player.money
+                    player.money = 0
+                    playerLands = list(filter(lambda l: l.owner == player.id, lands))
+                    #無地則破產
+                    if playerLands == None:
+                        player.status = 1
+                        loserCount += 1
+                        continue
 
-    print(_executionMode)
+                    playerLands.sort(key = lambda l: l.invest)
+                    totalPrice = 0
+                    for land in playerLands:
+                        totalPrice += land.invest
+                    #判斷持有土地總價是否足夠支付，不夠則全數轉移且破產
+                    if totalPrice < toll:
+                        for land in playerLands:
+                            land.owner = payee.id
+                        player.status = 1
+                        loserCount += 1
+                        continue
+                    #賣地給地主
+                    for land in playerLands:
+                        while player.money < toll:
+                            land.owner = payee.id
+                            toll -= land.invest
+                    if toll < 0:
+                        player.money += abs(toll)
+                        payee.money -= toll
 
-# 初始化
-initialize()
-# 取得玩家人數
-getNumOfPlayer()
-# 取得所有土地資料
-getLands()
+    print(f"round = {round} 結束")
+    print(json.dumps(players, default=lambda p: p.__dict__, indent = 4))
+    print()
+    print(json.dumps(lands, default=lambda p: p.__dict__, indent = 4))
+
+print(f"Game over, round = {round}")
+    
+
 
 
 
--- Monopoly/paraLib.py
+++ Monopoly/paraLib.py
@@ -6,11 +6,22 @@ config = configparser.ConfigParser()
 configPath = os.path.join(os.path.dirname(__file__), 'config.ini')
 config.read(configPath)
 
-# FUN:取得玩家人數 by config
-def getNumOfPlayer():
-    numOfPlayer = config.getint('Number of players', 'default')
-    return numOfPlayer
+# FUN:取得各項費率 by config
+def getRateDict():
+    rate_dict = dict(config.items('Rate'))
+    # 將所有的 value 轉換為 float
+    rate_dict = {key: float(value) for key, value in rate_dict.items()}
+    return rate_dict
 
+#Land obj
+class Land:
+    def __init__(self, land_dict):
+        self.name = land_dict['name']
+        self.price = int(land_dict['price'].strip())
+        self.owner = land_dict['owner']
+        self.level = int(land_dict['level'].strip())
+        self.location = int(land_dict['location'].strip())
+        self.invest = int(land_dict['invest'].strip())
 # FUN:取得土地設定 by config
 def getLands():
     lands = []
@@ -20,7 +31,67 @@ def getLands():
 
     for section in sections:
         if section.startswith('Land'):
-            land_dict = dict(config.items(section))
-            lands.append(land_dict)
+            land = Land(dict(config.items(section)))
+            lands.append(land)
 
     return lands
+
+#Player obj
+class Player:
+    def __init__(self, player_dict):
+        self.id = int(player_dict['id'].strip())
+        self.money = int(player_dict['money'].strip())
+        self.status = int(player_dict['status'].strip())
+        self.location = int(player_dict['location'].strip())
+# FUN:取得所有玩家資料 by config
+def getPlayers():
+    players = []
+
+   # 取得所有的Section名稱
+    sections = config.sections()
+
+    for section in sections:
+        if section.startswith('Player'):
+            player_dict = Player(dict(config.items(section)))
+            players.append(player_dict)
+
+    return players
+
+# ------未實作------
+#import enum
+
+## 執行模式
+#class ExecutionMode(enum.Enum):
+#    fullyAuto = 0 #連同參數設定皆為自動
+#    semiAuto = 1 #使用者輸入參數
+
+#_executionMode= ExecutionMode.fullyAuto
+
+# FUN:初始化執行模式
+#def initialize():
+#    print("選擇執行模式")
+#    print(" 0: 全自動模式(包含參數設定)")
+#    print(" 1: 由使用者輸入參數")
+
+#    #print("五秒內未輸入則為自動模式")
+#    #start_time = time.time()
+#    #while (time.time() - start_time) < 5:
+#    #    if m:=input("請輸入0 or 1"):
+#    #        userInput = m
+#    #        break
+#    #    else:
+#    #        userInput = "0"
+
+#    userInput=input("請輸入0 or 1\n")
+#    if userInput == "0":
+#        _executionMode = ExecutionMode.fullyAuto
+#    elif userInput == "1":
+#        _executionMode = ExecutionMode.semiAuto
+#    else :
+#        print("終止程式")
+#        exit()
+
+#    print(_executionMode)
+
+# 初始化
+#initialize()
