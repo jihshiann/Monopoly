ba148ce376e7c3d1d96adb4d9ad55b98dfc4da5e
Ji Xian, Tsai <jih-shiann@hotmail.com>
2023-03-09 22:54:36 +0800

    程式碼重構

--- Monopoly/Monopoly.py
+++ Monopoly/Monopoly.py
@@ -1,9 +1,8 @@
-﻿from textwrap import indent
-from paraLib import *
-import random
+﻿from Lib import *
+import Lib
 import json
 
-
+roundCount = 0
 #取得所有玩家資料
 players = getPlayers()
 #取得所有土地資料
@@ -11,85 +10,41 @@ lands = getLands()
 #取得各項費率
 rate_dict = getRateDict()
 
-loserCount = 0
-round = 0
-
 #遊玩中玩家>1位
-while loserCount < len(players)- 1:
-    round += 1
+while Lib.loserCount < len(players)- 1:
+    roundCount += 1
     for player in players:
         #1 = lose
         if player.status == 1:
             continue
+
         #0 = play
         if player.status == 0:
             #向前走1~4步
-            player.location = player.location + random.randint(1, 4)
-            if player.location > len(lands):
-                player.location = player.location % len(lands)
+            playerMove(player, len(lands))
             arriveLand = lands[player.location-1]
-            #判斷土地持有權
-            if arriveLand.owner == 'None':
-                #判斷現金是否足夠購買
-                if player.money >= arriveLand.price:
-                    player.money -= arriveLand.price
-                    arriveLand.owner = player.id
-                    arriveLand.level += 1
-                    arriveLand.invest += arriveLand.price
             
+            #無主地
+            if arriveLand.owner == 'None':
+                #買地
+                buyOrPass(player, arriveLand)
+
+            #自有土地
             elif arriveLand.owner == player.id:
-                #公式: price * rate * level
-                upgradeCost = rate_dict['upgrade'] * arriveLand.price * arriveLand.level
-                #判斷現金是否足夠升級
-                if player.money >= upgradeCost:
-                    player.money -= upgradeCost
-                    arriveLand.level += 1
-                    arriveLand.invest += upgradeCost
+                #升級
+                upgradeOrPass(player, arriveLand)
 
+            #他人土地
             else:
-                #公式 price * rate * level
-                toll = rate_dict['toll'] * arriveLand.price * arriveLand.level
-                payee = players[arriveLand.owner-1] #接受過路費對象(地主)
-                #判斷現金是否足夠付過路費
-                if player.money >= toll:
-                    payee.money += toll
-                    player.money -= toll
-                else:
-                    #先付清現金
-                    payee.money += player.money
-                    toll -=player.money
-                    player.money = 0
-                    playerLands = list(filter(lambda l: l.owner == player.id, lands))
-                    #無地則破產
-                    if playerLands == None:
-                        player.status = 1
-                        loserCount += 1
-                        continue
+                #接受過路費對象(地主)
+                payee = players[arriveLand.owner-1] 
+                #繳過路費
+                payForToll(player, payee, arriveLand, lands)
 
-                    playerLands.sort(key = lambda l: l.invest)
-                    totalPrice = 0
-                    for land in playerLands:
-                        totalPrice += land.invest
-                    #判斷持有土地總價是否足夠支付，不夠則全數轉移且破產
-                    if totalPrice < toll:
-                        for land in playerLands:
-                            land.owner = payee.id
-                        player.status = 1
-                        loserCount += 1
-                        continue
-                    #賣地給地主
-                    for land in playerLands:
-                        while player.money < toll:
-                            land.owner = payee.id
-                            toll -= land.invest
-                    if toll < 0:
-                        player.money += abs(toll)
-                        payee.money -= toll
-
-    print(f"round = {round} 結束")
-    print(json.dumps(players, default=lambda p: p.__dict__, indent = 4))
+    print(f"round = {roundCount} 結束")
+    print(json.dumps(players, default=lambda p: p.__dict__))
     print()
-    print(json.dumps(lands, default=lambda p: p.__dict__, indent = 4))
+    print(json.dumps(lands, default=lambda p: p.__dict__))
 
 print(f"Game over, round = {round}")
     
